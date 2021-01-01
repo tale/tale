e1a42712010916344a2cbcfa9579b5f390931f34
Aarnav Tale <46133336+tale@users.noreply.github.com>
2021-01-01 01:46:01 -0500

    Remove Dotfiles

--- shell/aliases.zsh
+++
@@ -1,9 +0,0 @@
-# Python aliases so I don't need to put a '3'
-alias python=/usr/local/bin/python3
-alias pip=/usr/local/bin/pip3
-
-# Theos stuff so that I don't suffer
-export THEOS=~/.theos
-alias i7="THEOS_DEVICE_IP=192.168.1.230"
-alias i6="THEOS_DEVICE_IP=192.168.1.249"
-alias final="FINALPACKAGE=1 DEBUG=0"
